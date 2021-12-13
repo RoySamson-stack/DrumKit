44f5f69a335b6eee63408382fec7ac084d1d8327
RoySamson-stack <rsam9009@gmail.com>
2021-12-13 11:44:13 +0300

    making the sound for thespecific buttons when clicked

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-    alert("I got clicked")
+    
+    this.style.color = "white"
+
   })
+
 }
 
 
+
+
+
+
+
+
+
+
+
+  //  //play souond when button is clicked
+  //  var audio = new Audio("sounds/crash.mp3");
+  //  audio.play();
