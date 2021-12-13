b071ac6adc87700bf0a9791a7d1af8bfc7d47327
RoySamson-stack <rsam9009@gmail.com>
2021-12-13 15:49:38 +0300

    adding a switch case to change sound according to the button pressed

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-
 var numberOfButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfButtons; i++){
-  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-    
-    this.style.color = "white"
+for (var i = 0; i < numberOfButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+      case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+      case "a":
+        var kickBass = new Audio("sounds/kick-bass.mp3");
+        kickBass.play();
+        break;
+      case "s":
+        var kickBass = new Audio("sounds/snare.mp3");
+        kickBass.play();
+        break;
+
+      case "d":
+        var kickBass = new Audio("sounds/tom-1.mp3");
+        kickBass.play();
+        break;
+      case "j":
+        var kickBass = new Audio("sounds/tom-2.mp3");
+        kickBass.play();
+        break;
+      case "k":
+        var kickBass = new Audio("sounds/tom-3.mp3");
+        kickBass.play();
+        break;
+
+      case "l":
+        var kickBass = new Audio("sounds/tom-4.mp3");
+        kickBass.play();
+        break;
+      default:
+    }
 
   })
 
@@ -20,6 +54,6 @@ for (var i = 0; i < numberOfButtons; i++){
 
 
 
-  //  //play souond when button is clicked
-  //  var audio = new Audio("sounds/crash.mp3");
-  //  audio.play();
+//  //play souond when button is clicked
+//  var audio = new Audio("sounds/crash.mp3");
+//  audio.play();
